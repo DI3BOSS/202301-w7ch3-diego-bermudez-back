6329ce75baab23baa1ffd45c60713aa5f2500760
DI3BOSS <diegofbermudez@gmail.com>
2023-02-22 23:24:02 +0100

    Implement gePokemons function controller

---
+++ src/server/controllers/pokemonControllers.ts
@@ -0,0 +1,17 @@
+import type { Request, Response } from "express";
+import CustomError from "../../CustomError/CustomError";
+import Pokemon from "../../database/models/pokemon";
+
+export const getPokemons = async (req: Request, res: Response) => {
+  try {
+    const pokemons = await Pokemon.find({});
+    res.status(200).json({ pokemons });
+  } catch (error) {
+    const customError = new CustomError(
+      error.message as string,
+      500,
+      "Couldn't retrieve pokemons"
+    );
+    throw customError;
+  }
+};
